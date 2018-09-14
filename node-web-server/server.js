dd0180b575e9c0d1a6d22740d5a3c47fd3efaad9
Pablo <pfeo87@gmail.com>
2018-09-13 22:10:43 -0300

    Express

---
+++ node-web-server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Pablo',
+        likes: ['Drawing','Singing']
+    })
+});
+
+app.get('/about', (req, res) => {
+   res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+   res.send({
+       errorMessage: 'Unable to handle request'});
+   });
+
+app.listen(3000);
