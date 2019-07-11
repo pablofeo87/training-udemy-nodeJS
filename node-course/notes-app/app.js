83a6cb8101714b17dcef4ab675639acd60c8f17a
Pablo <pablo.feo@globalshare.com.ar>
2019-07-11 17:25:23 -0300

    -

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,6 +1,39 @@
-const validator = require('validator');
-
+const yargs = require('yargs');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-console.log(getNotes());
-console.log(validator.isURL('https:/mead.io'));
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note');
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note');
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function() {
+        console.log('Listing out all notes');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading the note');
+    }
+})
+
+console.log(yargs.argv)
